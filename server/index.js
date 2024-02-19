cfc33e1a5d7919e6fc798479c11e76684a96115d
naso576 <70210241+naso576@users.noreply.github.com>
2024-02-19 10:45:39 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 const cors = require("cors")
-const http = require('http').Server(app);
+const http = require('https').Server(app);
 const PORT = 4000
 const socketIO = require('socket.io')(http, {
     cors: {
